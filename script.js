4a2a9387f4a8dc2761f9728e3d355ba674b7affe
Marlo0103 <e232921@edu.sbl.ch>
2022-11-16 14:48:37 +0100

    ka

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function handleKeyPress(ev) {
+    if (ev.code === "Space"){
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
+} else if (ev.key === "r") {
+    player.setAttribute(fill", "red")
+} else if (ev.key ===)
 }
-
 
 window.onkeydown = handleKeyPress
