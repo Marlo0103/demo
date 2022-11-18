39df4fa5deaae0d01f800a35eb5d3b5d53bc4aca
Marlo0103 <e232921@edu.sbl.ch>
2022-11-18 08:10:17 +0100

    lopp

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ function handleKeyPress(ev) {
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
+}
 } else if (ev.key === "r") {
     player.setAttribute("fill", "red")
-} else if (ev.key ===)
+} else if (ev.key ===) {
 }
 
-window.onkeydown = handleKeyPress
+function gameloop(){
+    const player = document.querySelector("#player")
+    player.setAttribute("cy", 100 * Math.random())
+    //Loop forever
+    window.requestAnimationFrame(gameloop);
+}
+gameloop()
+window.onkeydown = handleKeyPress   
