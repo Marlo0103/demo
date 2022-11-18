be7f83c580f794dcf8b83ce9cc401e087ca7c955
Marlo0103 <e232921@edu.sbl.ch>
2022-11-18 08:18:31 +0100

    added gravity

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ function handleKeyPress(ev) {
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
-}
 } else if (ev.key === "r") {
     player.setAttribute("fill", "red")
-} else if (ev.key ===) {
+} else if (ev.key === "g") {
+    player.setAttribute("fill", "green");
+}
 }
 
 function gameloop(){
     const player = document.querySelector("#player")
-    player.setAttribute("cy", 100 * Math.random())
+    let posY = parseInt(player.getAttribute("cy"))
+    player.setAttribute("cy", posY+1)
     //Loop forever
     window.requestAnimationFrame(gameloop);
 }
